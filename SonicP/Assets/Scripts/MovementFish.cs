1c663e01336289dd2ee07dcd7a55c479d1da15c9
EdwinYauyo <43685549+EdwinYauyo@users.noreply.github.com>
2018-11-02 19:14:16 -0500

    pececito mejorado y mejoras extra

--- SonicP/Assets/Scripts/MovementFish.cs
+++ SonicP/Assets/Scripts/MovementFish.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class MovementFish : MonoBehaviour
 {
-
+    
+    public float respawnDelay = 5f;
     public float JumpFish = 6.5f;
+
     private Rigidbody2D rb2d;
-    public float maxSpeed = 5f;
+    private Vector3 start;
+    
+    
     public bool JumpF = true;
     // Use this for initialization
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        start = transform.position;
     }
 
     // Update is called once per frame
     void FixedUpdate()
         
     {
-        if(JumpF)
+        JumpF = true;
+        if (JumpF)
         {
 
             rb2d.AddForce(Vector2.up * JumpFish, ForceMode2D.Impulse);
             rb2d.isKinematic = false;
-            JumpF= true;
             JumpF = false;
         }
+        
 
 
     }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("LimitFishUp"))
+        {
+            JumpFish = 0f;
+            transform.localScale = new Vector3(1f, -1f, 1f);
+        }
+        if (col.CompareTag("LimitFishDown"))
+        {
+            Invoke("Respawn", respawnDelay);
+        }
+        
+    }
+    void Respawn()
+    {
+        transform.position = start;
+        rb2d.velocity = Vector3.zero;
+        JumpFish = 1.2f;
+        transform.localScale = new Vector3(1f, 1f, 1f);
 
+    }
 }
